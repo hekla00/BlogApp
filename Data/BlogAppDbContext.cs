2cc6c2a5ff722f7df9d8ddcf634d587f5e2e33b5
Hekla Samuelsdottir <hekla.samuelsdottir@dell.com>
2024-10-24 13:40:56 +0100

    Add project files.

---
+++ Data/BlogAppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BlogApp.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Data
+{
+    public class BlogAppDbContext : DbContext
+    {
+        public BlogAppDbContext(DbContextOptions<BlogAppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        {
+
+            optionsBuilder.UseSqlServer("BlogAppDbConnectionString");
+
+        }
+    }
+}
+ 
